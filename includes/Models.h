5cde477fb02429013c055f62ec313c02f5bd140b
Artsiom Praulotski <144160855+ArtemPravlotskiy@users.noreply.github.com>
2025-04-30 14:58:42 +0300

    Ya bez ponyatia, no zapuskaetca

---
+++ includes/Models.h
@@ -0,0 +1,8 @@
+//
+// Created by pravl on 30.04.2025.
+//
+
+#ifndef MODELS_H
+#define MODELS_H
+
+#endif //MODELS_H
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by pravl on 30.04.2025.
+//
+
+int main() {
+    return 0;
+}
